9a81752225a087947c0d273b7a86dfb4a71751c9
shaukat <shadvoll@gmail.com>
2022-12-19 21:52:55 +0300

    Добавление getRow, getCol, getTile функций по матрице судоку. Функции работают по глобальному индекус index = i*m + j

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,7 @@
-N = M = 9
+import copy
+
+
+N = M = 9 #GLOBAL consts
 '''
     readSudoku(filename)
     Discription:
@@ -31,6 +34,35 @@ def printSudoku(matrix):
     for i in range(N):
         print(*matrix[i],sep=' ')
     
+
+def getCol(index,matrix):
+    i = index // M
+    j = index % M
+    res = []
+    for i in range(N):
+        res.append(matrix[i][j])
+    return res
+
+def getRow(index,matrix):
+    i = index // M
+    j = index % M
+    res = []
+    for j in range(M):
+        res.append(matrix[i][j])
+    return res
+
+def getTile(index,matrix):
+    row = index // M
+    col = index % M
+    i3 = row//3 * 3
+    j3 = col//3 * 3
+    res = []
+    for i in range(i3,i3+3):
+        for j in range(j3,j3+3):
+            res.append(matrix[i][j])
+    return res
+
+
 filename = 'sudoku-task0.txt'
 matrix = readSudoku(filename)
 printSudoku(matrix)
