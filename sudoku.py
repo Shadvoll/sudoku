be21d9b6d6270c53825cb36ff3ba35ec53a897fd
shaukat <shadvoll@gmail.com>
2022-12-19 21:47:00 +0300

    Добавление описания к функция readSudoku и printSudoku

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,15 @@
 N = M = 9
+'''
+    readSudoku(filename)
+    Discription:
+        Cчитывание матрицы судоку из filename (.txt файл). Пропускаются пустые строки.
+        Цифры должны идти через пробел. Ожидается на входе 9 х 9 матрица, нет проверки.
+    INPUT:
+        filename - название файла .txt для чтения
+    OUTPUT:
+        list of lists. Судоку матрица в виде списка списков цирф.
+
+'''
 def readSudoku(filename):
     with open(filename) as f:
         lines = f.readlines()
@@ -8,7 +19,14 @@ def readSudoku(filename):
             tmp = list(map(int,line.split()))
             res.append(tmp)
     return res
-
+'''
+    printSudoku( matrix )
+    Description:
+        Вывод матрицы судоку.
+    INPUT:
+        matrix - list of lists. Судоку матрица в виде списка списков цирф.
+    
+'''
 def printSudoku(matrix):
     for i in range(N):
         print(*matrix[i],sep=' ')
